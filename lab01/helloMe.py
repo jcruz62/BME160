ecbf5aa9c1b27c9ee01c7e0f06097c74b1440e37
jcruz62 <165008662+jcruz62@users.noreply.github.com>
2024-03-26 02:51:15 -0700

    Update helloMe.py

--- lab01/helloMe.py
+++ lab01/helloMe.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# Name: Jade Cruz(jcruz62)
+# Group Members: none
 
+"""
+Print out the most common statement in computer programming, Hello World.
+""" 
+
+''' this is a single line docstring '''
+class Announcer (str): # Announcer is now defined as a kind of python string.
+    def printMe (self): # creates a function printMe 
+        print (self)
+
+student = Announcer ('Hello Jade')
+dog = Announcer ('bark bark')
+student.printMe()
+dog.printMe()
