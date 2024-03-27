4d680702f1020c72629d5befef252c02dd434878
jcruz62 <165008662+jcruz62@users.noreply.github.com>
2024-03-26 18:15:52 -0700

    Create proteinParam.py

---
+++ lab03/proteinParam.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# Name: Your full name (cruzID)
+# Group Members: List full names (and cruzIDs) or “None”
+
+class ProteinParam :
+# These tables are for calculating:
+#     molecular weight (aa2mw), along with the mol. weight of H2O (mwH2O)
+#     absorbance at 280 nm (aa2abs280)
+#     pKa of positively charged Amino Acids (aa2chargePos)
+#     pKa of negatively charged Amino acids (aa2chargeNeg)
+#     and the constants aaNterm and aaCterm for pKa of the respective termini
+#  Feel free to move these to appropriate methods as you like
+
+# As written, these are accessed as class attributes, for example:
+# ProteinParam.aa2mw['A'] or ProteinParam.mwH2O
+
+    aa2mw = {
+        'A': 89.093,  'G': 75.067,  'M': 149.211, 'S': 105.093, 'C': 121.158,
+        'H': 155.155, 'N': 132.118, 'T': 119.119, 'D': 133.103, 'I': 131.173,
+        'P': 115.131, 'V': 117.146, 'E': 147.129, 'K': 146.188, 'Q': 146.145,
+        'W': 204.225,  'F': 165.189, 'L': 131.173, 'R': 174.201, 'Y': 181.189
+        }
+
+    mwH2O = 18.015
+    aa2abs280= {'Y':1490, 'W': 5500, 'C': 125}
+
+    aa2chargePos = {'K': 10.5, 'R':12.4, 'H':6}
+    aa2chargeNeg = {'D': 3.86, 'E': 4.25, 'C': 8.33, 'Y': 10}
+    aaNterm = 9.69
+    aaCterm = 2.34
+
+    def __init__ (self, protein):
+        pass
+
+    def aaCount (self):
+        pass
+
+    def pI (self):
+        pass
+
+    def aaComposition (self) :
+        pass
+
+    def _charge_ (self):
+        pass
+
+    def molarExtinction (self):
+        pass
+
+    def massExtinction (self):
+        myMW =  self.molecularWeight()
+        return self.molarExtinction() / myMW if myMW else 0.0
+
+    def molecularWeight (self):
+        pass
+
+# Please do not modify any of the following.  This will produce a standard output that can be parsed
+    
+import sys
+def main():
+    inString = input('protein sequence?')
+    while inString :
+        myParamMaker = ProteinParam(inString)
+        myAAnumber = myParamMaker.aaCount()
+        print ("Number of Amino Acids: {aaNum}".format(aaNum = myAAnumber))
+        print ("Molecular Weight: {:.1f}".format(myParamMaker.molecularWeight()))
+        print ("molar Extinction coefficient: {:.2f}".format(myParamMaker.molarExtinction()))
+        print ("mass Extinction coefficient: {:.2f}".format(myParamMaker.massExtinction()))
+        print ("Theoretical pI: {:.2f}".format(myParamMaker.pI()))
+        print ("Amino acid composition:")
+        
+        if myAAnumber == 0 : myAAnumber = 1  # handles the case where no AA are present 
+        
+        for aa,n in sorted(myParamMaker.aaComposition().items(), 
+                           key= lambda item:item[0]):
+            print ("\t{} = {:.2%}".format(aa, n/myAAnumber))
+    
+        inString = input('protein sequence?')
+
+if __name__ == "__main__":
+    main()
