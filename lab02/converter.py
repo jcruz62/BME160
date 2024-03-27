a488252ceb7567e9f659f59dd0afcac045f9df7b
jcruz62 <165008662+jcruz62@users.noreply.github.com>
2024-03-26 18:14:12 -0700

    Create converter.py

---
+++ lab02/converter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3 
+# Name: Jade Cruz(jcruz62)
+# Group Members: None
+
+'''
+Program docstring goes here
+'''
+short_AA = {
+            'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+            'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
+            'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+            'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
+            }
+
+long_AA = {value:key for key,value in short_AA.items()}
+
+RNA_codon_table = {
+# Second Base
+# U             C             A             G
+#U
+'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys',
+'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys',
+'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---',
+'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Trp',
+#C 
+'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg',
+'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
+'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
+'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
+#A
+'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser',
+'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
+'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
+'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
+#G
+'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly',
+'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
+'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
+'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
+}
+dnaCodonTable = {key.replace('U','T'):value for key, value in RNA_codon_table.items()}
+
+def main():
+    '''
+    Take in an input of a codon 3 or 1 letter and output the desired result. If input not found output unknown
+    AA = amino acid
+    DNA codon to 3 letter AA
+    RNA codon to 3 letter AA
+    3 letter AA to 1 letter AA
+    1 letter AA to 3 letter AA
+    '''
+    # take in a short/long DNA/RNA input and covert to all uppercase
+    data = input().upper()
+
+    # checks if data is in one of the dictionaries
+    if data in short_AA:
+        print("{} = {}".format(data, short_AA[data].upper()))
+    elif data in long_AA:
+        print("{} = {}".format(data, long_AA[data].upper()))
+    elif data in dnaCodonTable:
+        print("{} = {}".format(data, dnaCodonTable[data].upper()))
+    elif data in RNA_codon_table:
+        print("{} = {}".format(data, RNA_codon_table[data].upper()))
+    else:
+        # if not found in any dictionary, print unknown
+        print(f'{data} = unknown')
+
+main()
